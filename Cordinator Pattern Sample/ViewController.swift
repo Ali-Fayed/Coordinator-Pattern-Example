24baf3285058a7caacb5ab9b778a458a9a0bfe13
Ali Fayed <ali.fayed2666@gmail.com>
2021-11-18 01:39:33 +0200

    Initial Commit

---
+++ Cordinator Pattern Sample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cordinator Pattern Sample
+//
+//  Created by Ali Fayed on 18/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Cordinator Pattern SampleUITests/Cordinator_Pattern_SampleUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Cordinator_Pattern_SampleUITestsLaunchTests.swift
+//  Cordinator Pattern SampleUITests
+//
+//  Created by Ali Fayed on 18/11/2021.
+//
+
+import XCTest
+
+class Cordinator_Pattern_SampleUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
